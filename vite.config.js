5bcef9f8701253df86d3caa31f11b20730f3f748
Khayrullayevich Oyatullokh <edwardsnowden008@gmail.com>
2025-11-25 16:36:19 +0500

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -7,5 +10,7 @@ export default defineConfig({
       }
     }
   },
-  server: { port: 5173 }
+  server: {
+    port: 5173
+  }
 })
